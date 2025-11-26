2c52a108817299bf75e052a35eed06e46f1aa198
sorcoffee <24031554138@mhs.unesa.ac.id>
2025-11-26 13:45:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(page_title="Superstore Dashboard", layout="wide")
-st.title("Superstore Dashboard Professional")
+st.title("Superstore Dashboard")
 
 # -----------------------------
 # 1️⃣ Load Excel Data
